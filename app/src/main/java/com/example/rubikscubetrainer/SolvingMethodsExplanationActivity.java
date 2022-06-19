2dcbb3a5b520002172976c60e276d1dcfea5e244
Maor Malekan <maor.malekan@live.biu.ac.il>
2022-06-19 17:33:15 +0300

    repair bugs in links SolvingMethodsExplanationActivity

--- app/src/main/java/com/example/rubikscubetrainer/SolvingMethodsExplanationActivity.java
+++ app/src/main/java/com/example/rubikscubetrainer/SolvingMethodsExplanationActivity.java
@@ -12,8 +12,12 @@ public class SolvingMethodsExplanationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solving_methods_explanation);
-        TextView t = (TextView) findViewById(R.id.cfop_algorithm_explanation);
-        t.setMovementMethod(LinkMovementMethod.getInstance());
+        TextView t1 = (TextView) findViewById(R.id.beginner_algorithm_explanation);
+        TextView t2 = (TextView) findViewById(R.id.cfop_algorithm_explanation);
+        TextView t3 = (TextView) findViewById(R.id.koecimba_algorithm_explanation);
+        t1.setMovementMethod(LinkMovementMethod.getInstance());
+        t2.setMovementMethod(LinkMovementMethod.getInstance());
+        t3.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
 }
